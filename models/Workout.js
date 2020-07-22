ee047a7b56dab5f3f72dc6188898dc6e546f3d85
Eunah Kim <Eunahkim92@gmail.com>
2020-07-22 14:24:35 -0400

    Feat Workout model

--- models/Workout.js
+++ models/Workout.js
@@ -3,12 +3,34 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  day: Date.now(),
+  day: {
+    type: Date,
+    default: Date.now
+  },
   exercises: [
-
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter a type of exercise"
+      },
+      name: {
+          type: String,
+          trim: true,
+          required: "Enter a name of the exercise"
+      },
+      duration: {
+          type: Number,
+          required: "Enter the duration of the exercise"
+      },
+      weight: Number,
+      reps: Number,
+      sets: Number,
+      distance: Number
+    }
   ]
 });
 
-const Workout = mongoose.model("User", WorkoutSchema);
+const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
