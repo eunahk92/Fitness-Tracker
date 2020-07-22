d54139a4e873ebd5ba162023e3390f50c5c8c61e
Eunah Kim <Eunahkim92@gmail.com>
2020-07-22 16:46:47 -0400

    Remove unused package and require routes

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
+const logger = require("morgan");
 
-const PORT = process.env.PORT || process.env.EXPRESS_PORT;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-// app.use(require("./routes/api.js"));
+mongoose.connect("mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => console.log(`App running on localhost:${PORT}`));
