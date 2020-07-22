6750f2ff952adda96b5fea881330a8ea24098401
Eunah Kim <Eunahkim92@gmail.com>
2020-07-22 17:54:02 -0400

    Feat api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const Workout = require('../models/Workout');
+
+router.get('/api/workouts', (req, res) => {
+	Workout.find({})
+		.then(dbWorkout => res.json(dbWorkout))
+		.catch(err => res.json(err));
+});
+
+router.get('/api/workouts/range', (req, res) => {
+	Workout.find({})
+		.then(dbWorkout => res.json(dbWorkout))
+		.catch(err => res.json(err));
+});
+
+router.post('/api/workouts', ({ body }, res) => {
+    Workout.create(body)
+		.then(dbWorkout => res.json(dbWorkout))
+		.catch(err => res.json(err));
+});
+
+router.put('/api/workouts/:id', (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+	Workout.update(
+		{ _id: id },
+		{ $push: { exercises: body } }
+	).then(dbWorkout => res.json(dbWorkout))
+	.catch(err => res.json(err));
+});
+
+module.exports = router;
