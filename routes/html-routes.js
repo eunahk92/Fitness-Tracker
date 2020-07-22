63ae13f997be89e1f092f8db8a4226b677cf3b2c
Eunah Kim <Eunahkim92@gmail.com>
2020-07-22 16:47:15 -0400

    Refactor routes with Express

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,12 @@
 const path = require('path');
-const db = require('../models');
+const router = require("express").Router();
+const Workout = require('../models/Workout');
 
-module.exports = app => {
-    app.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, '../public/exercise.html')));
-    app.get('/stats', (req, res) => res.sendFile(path.join(__dirname, '../public/stats.html')));
-}
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
+});
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
+});
+
+module.exports = router;
