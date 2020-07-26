4b9fa6a22d89045f5754cfbebe04d9ff796fb8a0
Eunah Kim <Eunahkim92@gmail.com>
2020-07-25 21:11:52 -0400

    Refactor method for totalDuration

--- models/Workout.js
+++ models/Workout.js
@@ -2,44 +2,43 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        required: "Enter a type of exercise"
-      },
-      name: {
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [{
+        type: {
           type: String,
           trim: true,
-          required: "Enter a name of the exercise"
-      },
-      duration: {
-          type: Number,
-          required: "Enter the duration of the exercise"
-      },
-      weight: Number,
-      reps: Number,
-      sets: Number,
-      distance: Number
-    }
-  ]
-},
-{
-toJSON: {
-    virtuals: true
-}
-});
+          required: "Enter a type of exercise"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter a name of the exercise"
+        },
+        duration: {
+            type: Number,
+            required: "Enter the duration of the exercise"
+        },
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number
+      }]
+    },
+  {
+  toJSON: { virtuals: true }
+  }
+);
 
 WorkoutSchema.virtual("totalDuration").get(function () {
     return this.exercises.reduce((total, exercise) => {
         return total + exercise.duration;
     }, 0);
+});
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
